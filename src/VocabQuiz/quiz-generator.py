10f589ec3ced78587663b454b0605a41480a0f6a
JunbongJang <jjang@wpi.edu>
2019-06-23 01:31:53 -0400

    Updated words, added sentiment_analysis

--- src/VocabQuiz/quiz-generator.py
+++ src/VocabQuiz/quiz-generator.py
@@ -9,13 +9,28 @@ This will present me one word at a time until the end and test myself whether I 
 import re
 import random
 
-with open("gre_vocab_list.txt", encoding='utf8') as file:  # Use file to refer to the file object
-    one_vocab_per_line = file.read().replace(",", "\n", 1000)
-    raw_vocab_list = one_vocab_per_line.split('\n')
-    trimmed_vocab_list = list(map(lambda vocab: vocab.strip(), raw_vocab_list))
-    vocab_list = list(filter(lambda vocab: vocab != '', trimmed_vocab_list))   # remove empty string
-    vocab_list = list(filter(lambda vocab: re.search("^[0-9]+/[0-9]", vocab) is None, vocab_list))  # remove dates
-    random.shuffle(vocab_list)
-    for vocab in vocab_list:
-        print()
-        print(vocab)
+
+def getVocabList():
+    with open("gre_vocab_list.txt", encoding='utf8') as file:  # Use file to refer to the file object
+        one_vocab_per_line = file.read().replace(",", "\n", 1000)
+        raw_vocab_list = one_vocab_per_line.split('\n')
+        trimmed_vocab_list = list(map(lambda vocab: vocab.strip(), raw_vocab_list))
+        vocab_list = list(filter(lambda vocab: vocab != '', trimmed_vocab_list))   # remove empty string
+        vocab_list = list(filter(lambda vocab: re.search("^[0-9]+/[0-9]", vocab) is None, vocab_list))  # remove dates
+        random.shuffle(vocab_list)
+    return vocab_list
+
+
+import webbrowser
+def openWebBrowser(query):
+    webbrowser.open_new_tab('https://www.dictionary.com/browse/' + query)
+
+
+if __name__ == "__main__":
+    vocab_list = getVocabList()
+    for index, vocab in enumerate(vocab_list):
+        print(f'{index}: {vocab}')
+        openWebBrowser(vocab)
+        if index % 10 == 0 and index != 0:
+            user_answer = input("Would you like to continue?\n")
+            print('--------------------------------------------')
